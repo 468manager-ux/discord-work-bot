cf1dc63b38a7dc84b1161f3108f645e8c6b2313b
468manager-ux <468.manager@gmail.com>
2026-07-30 17:04:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
+const http = require('http');
+http.createServer((req, res) => res.end('Bot is running!')).listen(process.env.PORT || 3000);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
